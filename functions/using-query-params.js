17c1532aecd48be6f1b3770a572c6aff37eeffc7
dreadedhamish <hamish.erskine@gmail.com>
2022-02-17 21:05:05 +1100

    4

--- functions/using-query-params.js
+++ functions/using-query-params.js
@@ -1,29 +1,8 @@
-
-
 exports.handler = async (event, context) => {
-  const userName = event.queryStringParameters && event.queryStringParameters.name
-
-  const generateHtml = (name = 'there') => {
-    /** For security *always* escape output html
-      const safeValues = escapeHtml(name)
-    */
-    return `
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1>Hi ${name}</h1>
-      </body>
-    </html>`
-  }
+  const name = event.queryStringParameters.name || "World";
 
   return {
-    'statusCode': 200,
-    'headers': {
-      'Cache-Control': 'no-cache',
-      'Content-Type': 'text/html',
-    },
-    'body': generateHtml(userName)
-  }
-}
+    statusCode: 200,
+    body: `Hello, ${name}`,
+  };
+};
